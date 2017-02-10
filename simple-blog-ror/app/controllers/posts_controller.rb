dc4d9d8e6ade52a5c74d4b4e9d0f87aa148bfff7
jmsalcido <eljose152@gmail.com>
2017-02-10 16:22:24 -0700

    ckeditor

--- simple-blog-ror/app/controllers/posts_controller.rb
+++ simple-blog-ror/app/controllers/posts_controller.rb
@@ -12,8 +12,8 @@ class PostsController < ApplicationController
 
   def create
     byebug
-    @post = Post.new
-    if @post.save(post_params)
+    @post = Post.new(post_params)
+    if @post.save
       flash[:notice] = "Successfully created post!"
       redirect_to post_path(@post)
     else
